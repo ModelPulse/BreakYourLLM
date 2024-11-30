1ca5e9a3bc9f9b7b6e0f2a738213b0401db021cc
Ashrya Agrawal <ashryaagr@gmail.com>
2024-11-29 21:13:45 -0800

    fix deserialization

--- sources/models/common_interface.py
+++ sources/models/common_interface.py
@@ -21,21 +21,39 @@ class BaseTest:
     @classmethod
     def from_dict(cls, data):
         """Instantiate an object from a dictionary."""
-        obj = cls.__new__(cls)  # Bypass __init__
+        obj = cls.__new__(cls)  # Create a new instance without calling __init__
         for key, value in data.items():
-            # If the attribute is a nested Serializable object, deserialize it
-            if isinstance(getattr(cls, key, None), type) and issubclass(getattr(cls, key), BaseTest):
-                setattr(obj, key, getattr(cls, key).from_dict(value))
-            elif isinstance(getattr(cls, key, None), list):
-                # If it's a list, deserialize each item if it's a Serializable object
-                setattr(obj, key, [getattr(cls, key)[0].from_dict(item) if isinstance(getattr(cls, key)[0], BaseTest) else item for item in value])
+            attr_type = getattr(cls, key, None)  # Get the type of the attribute if defined
+            
+            if isinstance(value, dict):
+                # If value is a dictionary, recursively create an object
+                nested_class = getattr(cls, key, BaseTest)  # Default to BaseTest if no specific type is defined
+                setattr(obj, key, nested_class.from_dict(value))
+            elif isinstance(value, list):
+                # If value is a list, check if it's a list of nested objects
+                list_items = []
+                for item in value:
+                    if isinstance(item, dict):
+                        # Assume nested objects should be of BaseTest or derived type
+                        nested_item_class = getattr(cls, key, [BaseTest])[0]
+                        list_items.append(nested_item_class.from_dict(item))
+                    else:
+                        list_items.append(item)
+                setattr(obj, key, list_items)
             else:
+                # Set the value directly for simple types
                 setattr(obj, key, value)
         return obj
 
     @classmethod
-    def from_json(cls, json_string):
-        """Instantiate an object from a JSON string."""
-        print(json_string)
-        data = json.loads(json_string)
+    def from_json(cls, json_input):
+        """Instantiate an object from a JSON string or a dictionary."""
+        # Check if the input is already a dictionary
+        if isinstance(json_input, dict):
+            data = json_input
+        elif isinstance(json_input, str):
+            data = json.loads(json_input)
+        else:
+            raise TypeError("Input to from_json must be a JSON string or a dictionary.")
+        
         return cls.from_dict(data)
